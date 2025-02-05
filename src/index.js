e1f8e352d04f0c10c796d76b3702255d91a192d5
ManuFlorenca! <manuelaflorenca22@gmail.com>
2025-02-04 21:19:48 -0300

    Blocks (Switch case)

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const player1 = {
+    NOME : "Mario",
+    VELOCIDADE : 4,
+    MANOBRABILIDADE: 3,
+    PODER: 3,
+    PONTOS: 0,
+};
+
+const player2 = {
+    NOME : "Luigi",
+    VELOCIDADE : 3,
+    MANOBRABILIDADE: 4,
+    PODER: 4,
+    PONTOS: 0,
+};
+
+async function rollDice(){
+    return Math.floor(Math.random() * 6) + 1;
+};
+
+async function getRandomBlock(){
+    let random = Math.random()
+    let result
+
+    switch (true) {
+
+        case random < 0.33 :
+        result = "RETA";
+        break;
+
+        case random < 0.66 :
+            result = "CURVA";
+        break;
+
+        default:
+            result = "CONFRONTO"
+    }
+
+    return result
+};
+
+async function playRaceEngine(character1,character2) {
+
+    for (let round = 1; round <= 5; round++){
+
+        console.log(`🏁 Rodada ${round}`);
+
+        // sortear bloco
+
+        let block = await getRandomBlock()
+        console.log(`Bloco: ${block}`)
+    }
+};
+
+(async function main(){
+    console.log(`🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando...\n`);
+    await playRaceEngine(player1,player2);
+})();
